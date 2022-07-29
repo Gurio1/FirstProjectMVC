69c30c6981beb4c3234f51273bd334846814a6c2
Andrey <iliev.andreyy@gmail.com>
2022-07-29 19:41:06 +0300

    Rename

---
+++ AnimeWebSite/AnimeWebSite.Domain/Entities/Identiry/Users/ApplicationRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AnimeWebSite.Identity.Domain.Entities.Users
+{
+    public class ApplicationRole : IdentityRole<int>
+    {
+        public ICollection<ApplicationUser> Users { get; set; } = default!;
+    }
+}
