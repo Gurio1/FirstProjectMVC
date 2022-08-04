4a4d0c08919209f20cc3fc08c024bfece2bc2b99
Andrey <iliev.andreyy@gmail.com>
2022-08-05 00:09:19 +0300

    Moved into Contracts

--- AnimeWebSite/AnimeWebSite.Contracts/Authentication/ForgotPasswordViewModel.cs
+++ AnimeWebSite/AnimeWebSite.Contracts/Authentication/ForgotPasswordViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AnimeWebSite.Domain.Authentication
+namespace AnimeWebSite.Contracts
 {
     internal class ForgotPasswordViewModel
     {
