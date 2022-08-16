6e81e072235e70011cc2998b45204904b1673f39
Andrey <iliev.andreyy@gmail.com>
2022-08-16 22:38:17 +0300

    Try to add UserProfile

---
+++ AnimeWebSite/AnimeWebSite/Controllers/UserProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeWebSite.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public IActionResult UserProfile()
+        {
+
+            return View();
+        }
+    }
+}
