3b0f5a1759dc8981eb4769a8a970541c126c4b7c
Andrey <iliev.andreyy@gmail.com>
2022-08-16 22:36:11 +0300

    Renamed

---
+++ AnimeWebSite/AnimeWebSite/Controllers/AnimeController.cs
@@ -0,0 +1,34 @@
+﻿using AnimeWebSite.Infrastructure;
+using AnimeWebSite.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeWebSite.Controllers
+{
+    public class AnimeController : Controller
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public AnimeController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet("[controller]/{id}")]
+        public async Task<IActionResult> DetailsAsync(int id)
+        {
+            var anime = await _serviceManager.AnimeService.GetByIdAsync(id);
+            if (anime is null)
+            {
+                return NotFound(anime);
+            }
+
+            return View(anime);
+        }
+
+        [HttpGet("[controller]/Watching")]
+        public IActionResult Watching()
+        {
+            return View();
+        }
+    }
+}
