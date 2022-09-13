c2d3eb6e388c0097b65c97ce6a2348590c0535de
Andrey <iliev.andreyy@gmail.com>
2022-09-13 10:04:00 +0300

    Create folder where server will keep user image.

--- AnimeWebSite/AnimeWebSite.Services/FileSystemService.cs
+++ AnimeWebSite/AnimeWebSite.Services/FileSystemService.cs
@@ -14,9 +14,40 @@ namespace AnimeWebSite.Services
         {
             _appEnvironmen = appEnvironmen;
         }
-        public string Create(IFormFile file,string folderName,int? entityId = null)
+        public string Create(IFormFile file, string folderName, int? entityId = null)
         {
-            return "asd";
+            var userFolder = entityId?.ToString();
+
+            var path = $"/File/{folderName}";
+
+            var pathWithEnviroment = _appEnvironmen.WebRootPath;
+            if (file != null)
+            {
+                if (userFolder is null)
+                {
+                    path = Path.Combine(path, file.FileName);
+                }
+                else
+                {
+                    path = Path.Combine(path, userFolder);
+
+                    pathWithEnviroment = pathWithEnviroment + path;
+
+                    if (!Directory.Exists(pathWithEnviroment))
+                    {
+                        Directory.CreateDirectory(pathWithEnviroment);
+                    }
+
+                    path = path + "/" + file.FileName;
+                }
+
+                pathWithEnviroment = _appEnvironmen.WebRootPath + path;
+                using (var stream = new FileStream(pathWithEnviroment, FileMode.Create))
+                {
+                    file.CopyToAsync(stream);
+                }
+            }
+            return path;
         }
 
         public bool DeleteFile(string filePath)
