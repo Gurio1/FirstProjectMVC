508ac8017e19e8fc6a80b0e23dc4707caf533fc1
Andrey <iliev.andreyy@gmail.com>
2022-09-28 19:57:39 +0300

    Added new VM for comments and reviews

---
+++ AnimeWebSite/AnimeWebSite.Contracts/AnimeCommsAndReviews/AnimeReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeWebSite.Contracts.AnimeCommsAndReviews
+{
+    public sealed class AnimeReviewViewModel
+    {
+        public int UserId { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime PostedOn { get; set; }
+    }
+}
